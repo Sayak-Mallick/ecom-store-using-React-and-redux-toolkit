669138add9b856317be2f97022b15d22f6c7ab4f
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-13 04:58:15 +0530

    in progress

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
